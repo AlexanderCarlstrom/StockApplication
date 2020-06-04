26171b79c031558dadadc9cbd8d3e49862d1d2ee
Alexander <caarlstrom.alexander@gmail.com>
2020-06-04 09:57:13 +0200

    Setup auth page

--- frontend/src/components/Auth/Auth.js
+++ frontend/src/components/Auth/Auth.js
@@ -1,11 +1,80 @@
 import React from 'react';
 import './Auth.css';
-import Dashboard from '../Dashboard/Dashboard';
-import App from '../App';
 
 class AuthPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentPage: <Login />,
+      login: true,
+    };
+
+    this.showLoginForm = this.showLoginForm.bind(this);
+    this.showRegisterForm = this.showRegisterForm.bind(this);
+  }
+
+  showLoginForm = () => {
+    this.setState({
+      currentPage: <Login />,
+      login: true,
+    });
+  };
+
+  showRegisterForm = () => {
+    this.setState({
+      currentPage: <Register />,
+      login: false,
+    });
+    console.log('hello');
+  };
+
   render() {
-    return <div id="auth-page">Authentication Page</div>;
+    return (
+      <div id="auth-page">
+        <div className="header">
+          <button id="login-btn" disabled={this.state.login} onClick={this.showLoginForm}>
+            LOGIN
+          </button>
+          <button id="login-btn" disabled={!this.state.login} onClick={this.showRegisterForm}>
+            REGISTER
+          </button>
+        </div>
+        <div id="auth-form">{this.state.currentPage}</div>
+      </div>
+    );
+  }
+}
+
+class Login extends React.Component {
+  onSubmit = () => {
+    console.log('login');
+  };
+
+  render() {
+    return (
+      <form id="login" onSubmit={this.onSubmit}>
+        <input type="email" name="email" placeholder="Email" />
+        <input type="password" name="password" placeholder="Password" />
+        <button>Submit</button>
+      </form>
+    );
+  }
+}
+
+class Register extends React.Component {
+  onSubmit = () => {
+    console.log('register');
+  };
+
+  render() {
+    return (
+      <form id="register" onSubmit={this.onSubmit}>
+        <input type="email" name="email" placeholder="Email" />
+        <input type="password" name="password" placeholder="Password" />
+        <input type="password" name="repeat-password" placeholder="Repeat Password" />
+        <button>Submit</button>
+      </form>
+    );
   }
 }
 
